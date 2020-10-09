d5b7828c3d12bfa8b29ab3fa80321971c97b4161
Michele <michele@linux.com>
2020-10-09 17:02:05 -0400

    Create campaigns.py

---
+++ campaigns.py
@@ -0,0 +1,32 @@
+import requests
+
+def data_series(token,campaign_id):
+    header = {"Authorization": "Bearer " + str(token)}
+    endpoint = "https://rest.iad-03.braze.com/campaigns/data_series?campaign_id="+campaign_id+"&length=7&ending_at=2010-10-08T23:59:59-5:00"
+    post = requests.get(endpoint,headers=header)
+    return post.json()
+
+def campaigns_list(token):
+    header = {"Authorization": "Bearer " + str(token)}
+    endpoint = "https://rest.iad-03.braze.com/campaigns/list?page=0&include_archived=false&sort_direction=desc"
+    post = requests.get(endpoint,headers=header)
+    return post.json()
+
+def campaigns_details(token, campaign_id):
+    header = {"Authorization": "Bearer " + str(token)}
+    endpoint = "https://rest.iad-03.braze.com/campaigns/details?campaign_id="+campaign_id
+    post = requests.get(endpoint,headers=header)
+    return post.json()
+
+def braze(token):
+    header = {"Authorization": "Bearer " + str(token)}
+    endpoint = "https://rest.iad-03.braze.com/sends/data_series?campaign_id="+campaign_id+"&send_id=3456789&length=30&ending_at=2014-12-10T23:59:59-05:00"
+    post = requests.get(endpoint,headers=header)
+    return post.json()
+
+if __name__ == '__main__':
+    token = '111111111-2222-3333-4444-22222222'
+    campaign_id = 'aaaaaaaa-bbbb-cccc-dddd-eeeeeeeee'
+    data_series = data_series(token,campaign_id)        #### https://www.braze.com/docs/api/endpoints/export/campaigns/get_campaign_analytics/
+    details = campaigns_details(token, campaign_id)     #### https://www.braze.com/docs/api/endpoints/export/campaigns/get_campaign_details/
+    list = campaigns_list(token)                        #### https://www.braze.com/docs/api/endpoints/export/campaigns/get_campaigns/
